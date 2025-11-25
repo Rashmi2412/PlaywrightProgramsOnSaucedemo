6b386f4922820fb26cc6b21c0191317b53a7a5fc
Rashmi <rashmigupta2829@gmail.com>
2025-11-25 12:18:56 +0530

    Updated with LoginPage

--- tests/demo03.spec.js
+++ tests/demo03.spec.js
@@ -1,10 +1,9 @@
 const { test, expect } = require('@playwright/test');
+const {LoginPage} = require('../pages/LoginPage');
 test('should user able to login with provided credentials', async({ page }) => {
-
-    await page.goto ('https://www.saucedemo.com/');
-await page.getByPlaceholder('Username').fill('standard_user');
-await page.getByPlaceholder('Password').fill('secret_sauce');
-await page.getByRole('button', {name: 'Login'}).click();
+ const loginPage = new LoginPage(page);
+ await loginPage.goto();
+ await loginPage.login('standard_user', 'secret_sauce');
 expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 await page.locator('#react-burger-menu-btn').click();
 await page.waitForTimeout(3000);

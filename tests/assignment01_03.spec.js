1cc7f41ed02be7b16dd1fe668860b210a18dde2e
Rashmi <rashmigupta2829@gmail.com>
2025-11-25 14:09:42 +0530

    update test

--- tests/assignment01_03.spec.js
+++ tests/assignment01_03.spec.js
@@ -4,5 +4,6 @@ test('Assert that the heading text contains Example' , async({page}) => {
     await page.goto("https://example.com/");
     await expect(page.getByRole("heading", {level: 1})).toHaveText("Example Domain");
     await expect(page).toHaveTitle(/.*Example/);
+    //(/.*Example/) used for showing partial heading
     
 })
